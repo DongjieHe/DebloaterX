41605a2707e1ed0926be7a4068c4540ccc2adaee
DongjieHe <dongjieh@cse.unsw.edu.au>
2023-03-27 23:42:12 +0000

    update scripts

--- artifact/driver.sh
+++ artifact/driver.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-python3 run.py insens -all -out=output/run1/
-python3 run.py 2o -cd -cda=DEBLOATERX -all -out=output/run1/
-python3 run.py 2o -cd -all -out=output/run1/
-python3 run.py 2o -all -out=output/run1/
-python3 run.py 3o -cd -cda=DEBLOATERX -all -out=output/run1/
-python3 run.py 3o -cd -all -out=output/run1/
-python3 run.py 3o -all -out=output/run1/
-python3 run.py Z-2o E-2o -all -out=output/run1/
-python3 run.py Z-3o E-3o -all -out=output/run1/
+run="run1"
+python3 run.py insens -all -out=output/$run/
+python3 run.py 2o -cd -cda=DEBLOATERX -all -out=output/$run/
+python3 run.py 2o -cd -all -out=output/$run/
+python3 run.py 2o -all -out=output/$run/
+python3 run.py 3o -cd -cda=DEBLOATERX -all -out=output/$run/
+python3 run.py 3o -cd -all -out=output/$run/
+python3 run.py 3o -all -out=output/$run/
+python3 run.py Z-2o E-2o -all -out=output/$run/
+python3 run.py Z-3o E-3o -all -out=output/$run/
 
 
-python3 runbach.py insens -all -out=output-bach/run1/
-python3 runbach.py 2o -cd -cda=DEBLOATERX -all -out=output-bach/run1/
-python3 runbach.py 2o -cd -all -out=output-bach/run1/
-python3 runbach.py 2o -all -out=output-bach/run1/
-python3 runbach.py 3o -cd -cda=DEBLOATERX -all -out=output-bach/run1/
-python3 runbach.py 3o -cd -all -out=output-bach/run1/
-python3 runbach.py 3o -all -out=output-bach/run1/
-python3 runbach.py Z-2o E-2o -all -out=output-bach/run1/
-python3 runbach.py Z-3o E-3o -all -out=output-bach/run1/
+python3 runbach.py insens -all -out=output-bach/$run/
+python3 runbach.py 2o -cd -cda=DEBLOATERX -all -out=output-bach/$run/
+python3 runbach.py 2o -cd -all -out=output-bach/$run/
+python3 runbach.py 2o -all -out=output-bach/$run/
+python3 runbach.py 3o -cd -cda=DEBLOATERX -all -out=output-bach/$run/
+python3 runbach.py 3o -cd -all -out=output-bach/$run/
+python3 runbach.py 3o -all -out=output-bach/$run/
+python3 runbach.py Z-2o E-2o -all -out=output-bach/$run/
+python3 runbach.py Z-3o E-3o -all -out=output-bach/$run/
