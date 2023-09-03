320aa01185a138649168db9718a256f0355a3f59
DongjieHe <hedongjie15@gmail.com>
2023-09-03 13:40:25 +1000

    update dockderfile

--- artifact/debloaterX.py
+++ artifact/debloaterX.py
@@ -18,12 +18,12 @@ from util.debloaterX.xreductions import computeDebloaterXReductions
 analysisList = ['insens', '2o', 'E-2o', 'Z-2o', '2o+DC', '2o+D', '2o+DX', '3o', 'E-3o', 'Z-3o', '3o+DC', '3o+D', '3o+DX']
 
 # Main table for dacapo 2006
-dacapo2006out='output/run1/'
+dacapo2006out='sample/output/run1/'
 allPtaOutputs = Util.loadPtaOutputs(analysisList, bm.BENCHMARKS, dacapo2006out)
 genDebloaterXClientTable(allPtaOutputs, "dacapo2006.tex", bm.BENCHMARKS, analysisList, "Main results for \\texttt{DaCapo 2006}.")
 
 # Main table for dacapo-bach-9.12
-dacapobachout = 'output-bach/run1/'
+dacapobachout = 'sample/output-bach/run1/'
 allPtaOutputs = Util.loadPtaOutputs(analysisList, db.BENCHMARKS, dacapobachout)
 genDebloaterXClientTable(allPtaOutputs, "dacapobach.tex", db.BENCHMARKS, analysisList, "Main results for \\texttt{DaCapo-9.12}.")
 
